8e4ab64e0a6be36a040b935d311071b7b24abfd8
Jonathan Newman <jn202205@gmail.com>
2015-06-11 17:39:04 -0700

    pull cat class from server.rb

--- bin/server.rb
+++ bin/server.rb
@@ -1,65 +1,51 @@
 require 'webrick'
 require_relative '../lib/controller_base'
+require_relative '../lib/router'
+require_relative '../lib/db_connection'
+require_relative '../lib/sql_object'
+require_relative '../app/models/cat'
+require_relative '../app/models/status'
 
-class Cat
-  attr_reader :name, :owner
+DBConnection.reset
 
-  def self.all
-    @cat ||= []
-  end
-
-  def initialize(params = {})
-    params ||= {}
-    @name, @owner = params["name"], params["owner"]
-  end
-
-  def save
-    return false unless @name.present? && @owner.present?
-
-    Cat.all << self
-    true
-  end
-
-  def inspect
-    { name: name, owner: owner }.inspect
+class StatusesController < ControllerBase
+  def index
+    @statuses = Status.all
+    render :index
   end
 end
 
 class CatsController < ControllerBase
-  def create
-    @cat = Cat.new(params["cat"])
-    if @cat.save
-      self.session["count"] ||= 0
-      redirect_to("/cats")
-    else
-      render :new
-    end
-  end
-
   def index
-    self.session["count"] ||= 0
-    self.session["count"] += 1
     @cats = Cat.all
-    render :index
+    render(:index)
   end
 
   def new
-    self.session["count"] ||= 0
     @cat = Cat.new
-    render :new
+    render(:new)
   end
+
+  def create
+    @cat = Cat.new
+    render(:new)
+  end
+
+  def show
+    @cat = Cat.find(@params[:id])
+  end
+end
+
+router = Router.new
+router.draw do
+  get Regexp.new("^/cats$"), CatsController, :index
+  get Regexp.new("^/cats/(?<id>\\d+)$"), CatsController, :show
+  get Regexp.new("^/statuses$"), StatusesController, :index
 end
 
 server = WEBrick::HTTPServer.new(Port: 3000)
 server.mount_proc('/') do |req, res|
-  case [req.request_method, req.path]
-  when ['GET', '/cats'] || ['GET', '/']
-    CatsController.new(req, res, {}).index
-  when ['POST', '/cats']
-    CatsController.new(req, res, {}).create
-  when ['GET', '/cats/new']
-    CatsController.new(req, res, {}).new
-  end
+  route = router.run(req, res)
 end
 
 trap('INT') { server.shutdown }
